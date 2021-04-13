14e2ee9a1b67e913040969801911afcf4eb5205e
Ivan Reinaldo <reinaldoivan777@gmail.com>
2021-04-13 12:59:34 +0700

    update card styles

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ let userList = document.getElementById("users");
 function renderData(user) {
   let userCard = `<div class='card__user'>
   <img class="avatar" src="${user?.avatar}" class="img-fluid text-center"/>
-  <p>${user?.first_name}</p>
-  <h6>${user?.first_name} ${user?.last_name}</h6>
-  <p>${user?.email}</p>
+  <div class="username">${user?.first_name}</div>
+  <div class="fullname">${user?.first_name} ${user?.last_name}</div>
+  <div class="email">${user?.email}</div>
 </div>`;
   userList.innerHTML += userCard;
 }
